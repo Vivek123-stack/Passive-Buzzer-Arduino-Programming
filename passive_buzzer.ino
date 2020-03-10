64f8cb42c84c0a81d22894397478d471fdf13f1b
Vivek Pandey <60037963+Vivek123-stack@users.noreply.github.com>
2020-03-10 16:33:20 +0530

    Add files via upload
    final

---
+++ passive_buzzer.ino
@@ -0,0 +1,30 @@
+//Example Code for KY-006
+//Steps2Make.com 
+
+int buzzer = 8 ;// buzzer signal connected to this pin
+void setup ()
+{
+  pinMode (buzzer, OUTPUT) ;// set the digital IO pin mode, OUTPUT out of Wen
+}
+
+void loop ()
+{
+  unsigned char i, j ;// define variables
+  while (1)
+  {
+    for (i = 0; i <80; i++) // Wen a frequency sound
+    {
+      digitalWrite (buzzer, HIGH) ;// send voice
+      delay (1) ;// Delay 1ms
+      digitalWrite (buzzer, LOW) ;// do not send voice
+      delay (1) ;// delay ms
+    }
+    for (i = 0; i <100; i++) // Another frequency sound
+    {
+      digitalWrite (buzzer, HIGH) ;// send voice
+      delay (2) ;// delay 2ms
+      digitalWrite (buzzer, LOW) ;// do not send voice
+      delay (2) ;// delay 2ms
+    }
+  }
+}
